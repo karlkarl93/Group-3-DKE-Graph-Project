980a03782ff122ba779ae6bc013afedc6d666616
karlkarl93 <djkarl123@gmail.com>
2018-10-08 14:23:11 +0200

    Add files via upload

---
+++ Fishy.java
@@ -0,0 +1,10 @@
+public class Fishy {
+	public static void main ( String[] args ) {
+		System.out.println("           ,--,_        ");
+		System.out.println("    __   _\\\\.---'-.     ");
+		System.out.println("   \\ '.-\"      // o\\    ");
+		System.out.println("   /_.'-._     \\\\  /    ");
+		System.out.println("          '\"---(/\"'     ");
+		System.out.println( "This sure looks fishy...");
+		}
+}
