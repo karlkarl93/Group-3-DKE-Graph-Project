13f7e36b40aa9d3cf2b27085a959d2bc047befe4
Exodia7 <43958771+Exodia7@users.noreply.github.com>
2018-10-10 14:24:09 +0200

    Added functionalities
    Added a parameter time to give to chromaticNumber(), and chromaticNumber() gives half the time to Karloid() to find the number, then if it didn't find the chromatic number in the given time, it calls BottomUpWorkingMethod() to calculate the chromaticNumber(), giving it the other half of the time.
    
    If neither of the 2 gives a solution in the given time, then it calls upperBound(), and returns the upper bound for the graph

--- BottomUp.java
+++ BottomUp.java
@@ -141,13 +141,29 @@ public class BottomUp {
 			
 			if (DEBUG && (notUsed > 0)) System.out.println("There are " + (n-notUsed) + " actually used points.");
 			
-			int chromaNumber = chromaticNumber(e, n, m, notUsed);
 			
-			System.out.println("The chromatic number of Karloid is " + chromaNumber);
+			int secsTime = 20;
+			//Need to add a verification to know if its the chromatic numbers or the upper bound
+			int chromaNumber = chromaticNumber(e, n, m, notUsed, secsTime);
 			
+			if (chromaNumber != -1) {
+				System.out.println("The chromatic number is " + chromaNumber);
+			}
+			else {
+				int upBound = upperBound(e, n);
+				
+				System.out.println("The upper bound is " + upBound);
+			}
 		}
 	
-	public static int chromaticNumber(ColEdge[] e, int n, int m, int notUsed) {
+	/*
+	n = number of points (vertices)
+	m = number of edges (connections)
+	e[0].u = 1
+	e[0].v = 2
+	*/
+	
+	public static int chromaticNumber(ColEdge[] e, int n, int m, int notUsed, int time) {
 		//If empty graph, then we need only 1 color
 		if (m == 0) {
 			if (DEBUG) {
@@ -155,7 +171,7 @@ public class BottomUp {
 			}
 			return 1;
 		}
-		//Else ifwe have a complete graph (maximum number of connections), then we need n colors
+		//Else if we have a complete graph (maximum number of connections), then we need n colors
 		else if (m == ((n-notUsed)*(n-notUsed-1)/2)) {
 			if (DEBUG) {
 				if (notUsed == 0) {
@@ -169,11 +185,11 @@ public class BottomUp {
 		}
 		//Otherwise, it's complicated
 		else {
-			return BottomUp(e, n, m);
+			return BottomUp(e, n, time);
 		}
 	}
 	
-	public static int BottomUp (ColEdge[] e, int n, int m) {
+	public static int BottomUp (ColEdge[] e, int n, int time) {
 		//Start with the maximum size array,
 		int[][] result = new int[n-1][n];
 		// Note: could be optimized to use limited size, then enlarge if needed
@@ -183,31 +199,53 @@ public class BottomUp {
 			result[0][i-1] = i;
 		}
 		
-		return Karloid(e, n, m, result);
+		int res = Karloid(e, n, result, time/2);
+		
+		if (res == -1) {
+			System.out.println("Interrupted Karloid");
+			long start = System.currentTimeMillis();
+			res = BottomUpWorkingMethod(e, n, 0, result, n-1, start, time/2);
+			
+			if (res == -1) {
+				System.out.println("Interrupted BottomUpWorkingMethod too");
+				return -1;
+			}
+			else {
+				return res;
+			}
+		}
+		else {
+			return res;
+		}
 	}
 	
 	/** Chromatic number method
 	*/
-	public static int Karloid (ColEdge[] e, int n, int m, int[][] result) {
+	public static int Karloid (ColEdge[] e, int n, int[][] result, int time) {
+		long start = System.currentTimeMillis();
 		int[] moved = new int[n];
 		for (int i = 0; i < result.length; i ++) {
 			for (int j = 0; j < e.length; j ++) {
+				if (((double)((System.currentTimeMillis()-start)/Math.pow(10, 3))) > time) {
+					return -1;
+				}
+				
 				if ((ArrayContains(result[i], e[j].u)) && (ArrayContains(result[i], e[j].v))) {
 					if (i == 0) {
-						result[i+1] = Add(result[i+1], e[j].v);
-						result[i] = Erase(result[i], e[j].v);
+						Add(result[i+1], e[j].v);
+						Erase(result[i], e[j].v);
 					}
 					else {
 						if (ArrayContains(moved, e[j].v)) {
-							result[0] = Add(result[0], e[j].v);
-							result[i] = Erase(result[i], e[j].v);
-							moved = Erase(moved, e[j].v);
+							Add(result[i-2], e[j].v);
+							Erase(result[i], e[j].v);
+							Erase(moved, e[j].v);
 							i = 0;
 						}
 						else {
-							result[i+1] = Add(result[i+1], e[j].v);
-							result[i] = Erase(result[i], e[j].v);
-							moved = Add(moved, e[j].v);
+							Add(result[i+1], e[j].v);
+							Erase(result[i], e[j].v);
+							Add(moved, e[j].v);
 						}
 					}
 				}
@@ -221,64 +259,103 @@ public class BottomUp {
 		return usedColors(result);
 	}
 	
-	/*
-	n = number of points (vertices)
-	m = number of edges (connections)
-	e[0].u = 1
-	e[0].v = 2
+	/** 
+	This method should separate the two points connected by e[edgeToCheck], if they are in the same result[x] array (that is, they share the same color).
+	It should then try each possible separation (moving e[edgeToCheck].u to any other color and moving e[edgeToCheck].v to any other color).
+	In case we can't move 
 	
-	public static int BottomUpWorkingMethod (ColEdge[] e, int n, int m, int edgeToCheck, int[][] result) {
-		//Need to separate the numbers that are connected by e[edgeToCheck]
-		
-		//Loop through the colors, if they are not filled with 0's (meaning they have no vertices)
-		int j = 0;
-		//The boolean represents the fact if we have found a case where we need to do something because of the current edge
-		boolean noEdgeCase = true
-		while ((j < result.length) && (! arrayEmpty(result[j])) && (noEdgeCase)) {
-			//If they contain both values of the edge,
-			if (ArrayContains(result[j], e[edgeToCheck].u) && ArrayContains(result[j], e[edgeToCheck].v)) {
-				//Separate the numbers
-				
-				//n = number of colors used until now
-				int n = usedColors(result);
-				int min = -1;
-				for (int i = 0; i < n; i ++) {
-					int[][] option1 = Move(result[][], j, e[edgeToCheck].u, j+i+1);
-					int[][] option2 = Move(result[][], j, e[edgeToCheck].v, j+i+1);
-					
-					int numColors1 = BottomUpWorkingMethod(e, n, m, edgeToCheck+1, option1);
-					int numColors2 = BottomUpWorkingMethod(e, n, m, edgeToCheck+1, option2);
-					
-					if (numColors1 < min) {
-						min = numColors1;
-					}
-					if (numColors2 < min) {
-						min = numColors2;
-					}
-				}
-				
-				//Then, end the while-loop because we found the case where we need to take an action because of the edge
-				noEdgeCase = false
-			}
-			else {
-				j ++;
-			}
+	If the two points connected by e[edgeToCheck] are in different colors, it should just call itself to check the next edge, without changing result[][]
+	
+	Finally, if edgeToCheck = e.length, it should just return the number of colors the result has
+	*/
+	public static int BottomUpWorkingMethod (ColEdge[] e, int n, int edgeToCheck, int[][] result, int min, long start, int time) {		
+		//First, check if the time is up
+		if (((double)((System.currentTimeMillis()-start)/Math.pow(10,3))) > time) {
+			return -1;
 		}
 		
-		
-		
-		return result;
+		//Normally, this if will only trigger when edgeToCheck == e.length, but for security, I say if edgeToCheck >= e.length
+		if (edgeToCheck >= e.length) {
+			if (DEBUG) System.out.printf("Possible result (%d colors): %s\n\n", usedColors(result), Arrays.deepToString(result));
+			
+			//Returns the amount of colors of the color assignment (result)
+			return usedColors(result);
+		}
+		else {
+			int j = 0;
+			//If result[j = 0] contains both values of the edge, (because it is not possible to have two edge points in another color)
+			if (ArrayContains(result[j], e[edgeToCheck].u) && ArrayContains(result[j], e[edgeToCheck].v)) {
+				//Separate the numbers
+
+				//n = number of colors used until now
+				int numCols = usedColors(result);
+
+				//Only try out possible options to continue if you currently have less colors than the current minimum
+				if (numCols < min) {
+					for (int i = 1; i <= numCols; i ++) {
+						boolean oneOptionFound = false;
+
+						//We only try the creation of a new color if there is no possible option to add the item toMove to an existing color
+						if ((i < numCols) | (! oneOptionFound)) {
+							if (i < result.length) {
+								if (MoveIsPossible(e, result, e[edgeToCheck].u, i)) {
+									//If option1 here is possible, then try it out
+									int[][] option1 = Move(result, j, e[edgeToCheck].u, i);
+									int numColors1 = BottomUpWorkingMethod(e, n, edgeToCheck+1, option1, min, start, time);
+									option1 = Move(result, i, e[edgeToCheck].u, j);
+
+									if (numColors1 < min) {
+										min = numColors1;
+									}
+
+									oneOptionFound = true;
+								}
+
+								//If option2 is possible, try it out and 
+								if (MoveIsPossible(e, result, e[edgeToCheck].v, i)) {
+									int[][] option2 = Move(result, j, e[edgeToCheck].v, i);
+									int numColors2 = BottomUpWorkingMethod(e, n, edgeToCheck+1, option2, min, start, time);
+									option2 = Move(result, i, e[edgeToCheck].v, j);
+
+									if (numColors2 < min) {
+										min = numColors2;
+									}
+
+									oneOptionFound = true;
+								}
+							}
+						}
+					}
+				}
+				return min;
+			}
+			//This gets only executed if the edge vertices are not in the same color, 
+			//therefore, nothing needs to be done to result
+			return BottomUpWorkingMethod(e, n, edgeToCheck+1, result, min, start, time);
+		}
 	}
-	*/
+	
+	public static int upperBound(ColEdge[] e, int n) {
+		//Start with the maximum size array,
+		int[][] result = new int[n-1][n];
+		// Note: could be optimized to use limited size, then enlarge if needed
+		
+		//Initiate all the vertices on the same colour
+		for (int i=1; i <= n; i ++) {
+			result[0][i-1] = i;
+		}
+		
+		return KarloidUpperBound(e, n, result);
+	}
 	
 	/** Upper bound method for the chromatic number
 	*/
-	public static int KarloidUpperBound (ColEdge[] e, int n, int m, int[][] result) {
+	public static int KarloidUpperBound (ColEdge[] e, int n, int[][] result) {
 		for (int i = 0; i < result.length; i ++) {
 			for (int j = 0; j < e.length; j ++) {
 				if ((ArrayContains(result[i], e[j].u)) && (ArrayContains(result[i], e[j].v))) {
-					result[i+1] = Add(result[i+1], e[j].v);
-					result[i] = Erase(result[i], e[j].v);
+					Add(result[i+1], e[j].v);
+					Erase(result[i], e[j].v);
 				}
 			}
 		}
@@ -288,36 +365,64 @@ public class BottomUp {
 		return usedColors(result);
 	}
 	
-	public static int[] Add (int[] array, int element) {
-		int i = 0;
-		boolean notAdded = true;
-		while ((i < array.length) && (notAdded)) {
-			if (array[i] == 0) {
-				array[i] = element;
-				notAdded = false;
-			}
-			else {
-				i ++;
+	/**
+	Checks if I can move item toMove from array[origin] to array[destination], that is, without having any element of array[destination] sharing an edge with toMove
+	*/
+	public static boolean MoveIsPossible (ColEdge[] e, int[][] array, int toMove, int destination) {
+		//As I should only need to move points from the 1st color to another one, I can assume that the position
+		//of the point is array[0][toMove-1]
+		boolean movePossible = true;
+		int j = 0;
+		
+		//If the destination array is not empty, we need to check if we can add toMove in it, otherwise, it is a new color we create
+		if (! arrayEmpty(array[destination])) {
+			while ((j < e.length) && (movePossible)) {
+				//For each edge, if the item to move is one of the two points, check if the other item is in the array I try to move the item to
+				if ((e[j].u == toMove) | (e[j].v == toMove)) {
+					if ((ArrayContains(array[destination], e[j].u)) | (ArrayContains(array[destination], e[j].v))) {
+						movePossible = false;
+					}
+				}
+				j ++;
 			}
 		}
 		
+		return movePossible;
+	}
+	
+	/** 
+	Should move item toMove from array[origin] to array[destination].
+	The User should have already checked if the move is possible (that is, doesn't bring together any edge-connected-points) using movePossible()
+	*/
+	public static int[][] Move(int[][] array, int origin, int toMove, int destination) {
+		int i = 0;
+		boolean notFoundToMove = true;
+		//For each item in the origin array, while we have not found the item we need to move
+		while ((i < array[origin].length) && (notFoundToMove)) {
+			if (array[origin][i] == toMove) {
+				//Once we find the item we need to move,
+				notFoundToMove = false;
+				
+				if (DEBUG) System.out.printf("Moving %d from result[%d] to result[%d] \n", toMove, origin, destination);
+				
+				//Deletes the original toMove
+				array[origin][i] = 0;
+				//And adds it in array[destination]
+				Add(array[destination], toMove);
+			}
+			i ++;
+		}
+		
+		//Return the modified array
 		return array;
 	}
 	
-	public static int[] Erase (int[] array, int element) {
-		int i = 0;
-		boolean notErased = true;
-		while ((i < array.length) && (notErased)) {
-			if (array[i] == element) {
-				array[i] = 0;
-				notErased = false;
-			}
-			else {
-				i ++;
-			}
-		}
-		
-		return array;
+	public static void Add (int[] array, int element) {
+		array[element-1] = element;
+	}
+	
+	public static void Erase (int[] array, int element) {
+		array[element-1] = 0;
 	}
 	
 	public static int usedColors (int[][] array) {
