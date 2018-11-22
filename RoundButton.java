afd27599e686474eb74339bbc8b00ca1fb10424c
Shunian2892 <43992106+Shunian2892@users.noreply.github.com>
2018-11-22 14:54:53 +0100

    Add files via upload

--- RoundButton.java
+++ RoundButton.java
@@ -1,7 +1,11 @@
 import java.awt.*;
 import java.awt.geom.*;
-import javax.swing.*;
 import java.awt.event.*;
+import java.awt.Component;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+import javax.swing.JColorChooser;
 
 public class RoundButton extends JButton
 {
@@ -53,15 +57,30 @@ public class RoundButton extends JButton
     // test it 
     public static void main(String[] args)
     {
-       JButton b = new RoundButton("I'm a button, change my mind");
-        b.setBackground(Color.white);
-
+        JButton b = new RoundButton("I'm a button, change my mind");
+       
         JFrame frame = new JFrame("BUTTONNNSSSS");
         frame.add(b);
         frame.setSize(500, 500);
         frame.setVisible(true);
         frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
 
+        ActionListener actionListener = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                Color initialBackground = b.getBackground();
+                Color background = JColorChooser.showDialog(null, "Change the color", initialBackground);
+                    if (background != null)
+                    {
+                        b.setBackground(background);
+                    } 
+            }
+        };
+
+        b.addActionListener(actionListener);
+        
+         
+    }
 }
