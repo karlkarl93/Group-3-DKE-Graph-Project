4c51b2411dd8c42f5b701c6daab7548c1aad88fe
Exodia7 <43958771+Exodia7@users.noreply.github.com>
2019-01-09 14:48:34 +0100

    Uploaded files from Phase 2 and new java files for Phase 3

---
+++ Project 1.3/MapConnections.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class MapConnections {
+	protected static int n;							//the number of vertices
+	protected static int[] numAdjacentVertices;		//the number of adjacent vertices of each vertex
+	
+	/** Constructs a "dictionary" such that 
+			- the keys are the vertices and 
+			- the values are arrays containing the indexes of the vertices they are connected to
+		
+		@param g, the Graph of which to compute the connections
+	*/
+	public static Map<Integer, int[]> connections (Graph g) {
+		Edge[] e = g.getEdges();
+		n = g.getN();
+		
+		Map<Integer, int[]> connections = new HashMap<Integer,int[]>();
+		numAdjacentVertices = new int[n+1];
+		for(int i = 0; i < e.length; i ++) {
+			if (connections.containsKey(e[i].u)) {
+				connections.put(e[i].u, Add(e[i].u, connections.get(e[i].u), e[i].v));
+			}
+			else {
+				connections.put(e[i].u, Add(e[i].u, new int[n-1], e[i].v));
+			}
+			numAdjacentVertices[e[i].u] ++;
+			
+			if (connections.containsKey(e[i].v)) {
+				connections.put(e[i].v, Add(e[i].v, connections.get(e[i].v), e[i].u));
+			}
+			else {
+				connections.put(e[i].v, Add(e[i].v, new int[n-1], e[i].u));
+			}
+			numAdjacentVertices[e[i].v] ++;
+		}
+		
+		for (int i = 1; i <= n; i ++) {
+			int[] c = connections.get(i);
+			
+			//Create a new array that has the real length of the adjacentVertices of this vertex (when disregarding the 0's)
+			int[] newC = new int[numAdjacentVertices[i]];
+			for (int j = 0; j < newC.length; j ++) {
+				//And add it the real vertice-indexes (of this vertex's adjacentVertices)
+				newC[j] = c[j];
+			}
+			
+			//Finally, set the connections of this vertex to its correct size
+			connections.put(i, newC);
+		}
+		
+		return connections;
+	}
+	
+	/** Shows the connections of a certain Graph
+		
+		@param c, the map containing the connections of the different vertices of the Graph
+	*/
+	public static void showConnections (Map<Integer, int[]> c) {
+		for (int i = 1; i <= n; i ++) {
+			System.out.printf("%d: %s \n", i, Arrays.toString(c.get(i)));
+		}
+	}
+	
+	/** Auxiliary method for connections() method
+		
+		@param vertexIndex, the index of the vertex we want to modify the adjacentVertices array of
+		@param array, the current adjacentVertices array of the vertex
+		@param element, the new vertex-index to be added to the adjacentVertices array
+		
+		@return the new array with element added to it
+	*/
+	public static int[] Add (int vertexIndex, int[] array, int element) {
+		array[numAdjacentVertices[vertexIndex]] = element;
+ 		
+		return array;
+ 	}
+}
---
+++ Project 1.3/UpperBound.java
@@ -0,0 +1,162 @@
+import java.io.*;
+import java.util.*;
+
+/*
+	NOTE !!!!! If we are to run multiple graphs in one run, then the system of using global "eigenvalues" variables needs to be modified ...
+				And we'll probably need to recompute the eigenvalues each time
+*/
+
+public class UpperBound {
+	public static int[] eigenvalues = new int[0];
+	public static Map<Integer, int[]> connections;
+	
+	/** The general method that calls the other hidden methods that compute the upper bounds
+	
+		@param g, the Graph of which we try to compute the upper bound(s)
+		
+	Methods available:
+		- upperBoundMaxDegree
+		- upperBoundTopDown
+		- upperBoundEigenvalues --- Not working ---
+	*/
+	public static int upperBound(Graph g, int method) {
+		if (method == 1) {
+			return upperBoundMaxDegree(g);
+		} else {
+			return upperBoundTopDown(g);
+		}
+	}
+	
+	/** One of the UpperBound methods --- Not working ---
+		Computes the eigenvalues of the adjacency matrix of the Graph (if it has not already been computed, and then)
+	*/
+	public static int upperBoundEigenvalues (Graph g) {
+		if (eigenvalues.length == 0) {
+			int[][] adj = new int[g.getN()][g.getN()];
+			Edge[] edges = g.getEdges();
+
+			//Construct the adjacency matrix
+			for (int i = 0; i < edges.length; i ++) {
+				adj[edges[i].u][edges[i].v] ++;
+				adj[edges[i].v][edges[i].u] ++;
+			}
+
+			//Compute its eigenvalues
+			int[] eigenvalues = new int[g.getN()];
+
+			//
+			// Problemo ... -------------------------------------------------------------------------------------
+			// Would need to use a library to calculate the eigenvalues ...
+			// e.g. JAMA (http://math.nist.gov/javanumerics/jama/)
+			//
+			
+			lowerBound.eigenvalues = eigenvalues;
+		}
+		
+		//Search out the smallest and the biggest
+		int max = eigenvalues[0];
+		for (int i = 1; i < eigenvalues.length; i ++) {
+			if (eigenvalues[i] > max) max = eigenvalues[i];
+		}
+		
+		return max + 1;
+	}
+	
+	/** One of the UpperBound methods
+		First, constructs a map of connections of the vertices using MapConnections.connections(), 
+		and then goes through the "adjacentVertices" of each vertex and keeps track of the maximum number of adjacentVertices encountered
+		Finally, returns the final maximum+1 as upperBound
+	*/
+	public static int upperBoundMaxDegree (Graph g) {
+		connections = TestGraph.connections;
+		int max = connections.get(1).length;
+		for (int i = 2; i <= g.getN(); i ++) {
+			if (max < connections.get(i).length) {
+				max = connections.get(i).length;
+			}
+		}
+		
+		return max + 1;
+	}
+		
+	/** One of the upperBound methods
+		It starts by having all vertex take a different color, then tries to "reunite" those vertices who are not connected by making a certain vertex give its colour to others
+	*/
+	public static int upperBoundTopDown (Graph g) {
+		// Compiles an array of vertex objects that store index, edges and colour information
+		Vertex[] vertices = g.vertices;
+		
+		for (int i = 1; i < vertices.length; i ++) {
+			vertices[i].color = i;
+		}
+		
+		int provisionIndex = 1; // referring to the current vertex that tries to provide its colour to the other vertices
+		int numberOfVariableVertices = vertices.length; // referring to the vertices that have not yet provided or inherited their colour
+		int upperBound = vertices.length;
+		
+		// Descend the upper bound
+		while (numberOfVariableVertices > 1) { // trying to descend as long as there are vertices that have a variable colour
+			
+			if (!vertices[provisionIndex].isVariable) { provisionIndex ++; } // jumping to the next vertex if the current vertex is frozen
+			else {
+				
+				boolean didProvideColour = false;
+				
+				// Try to disseminate the colour of the current providing vertex
+				for (int candidateIndex = 1; candidateIndex < vertices.length; candidateIndex++) {
+					
+					// Select the vertices that are variable and different from the current providing vertex. They are identified as candidates to inherit the colour of the current providing vertex
+					if (vertices[candidateIndex].isVariable && candidateIndex != provisionIndex) {
+						
+						// Check whether the candidate vertex can be recoloured
+						boolean vertexCanBeRecoloured = true;
+						int edgeIndex = 0;
+
+						// Disqualify candidate if it edges with the providing vertex
+						while (vertexCanBeRecoloured && edgeIndex < vertices[provisionIndex].adjacentVertices.length) {
+							if (candidateIndex == vertices[provisionIndex].adjacentVertices[edgeIndex].vertexIndex) { vertexCanBeRecoloured = false; }
+							edgeIndex ++;
+						}
+						
+						// Disqualify candidate if it edges with a third vertex that has the same colour as the providing vertex
+						edgeIndex = 0;
+						while (vertexCanBeRecoloured && edgeIndex < vertices[candidateIndex].adjacentVertices.length) {
+							int thirdVertexIndex = vertices[candidateIndex].adjacentVertices[edgeIndex].vertexIndex;
+							if (vertices[thirdVertexIndex].color == vertices[provisionIndex].color) { vertexCanBeRecoloured = false; }
+							edgeIndex ++;
+						}
+						
+						// Recolour the candidate if possible
+						if (vertexCanBeRecoloured) {
+							vertices[candidateIndex].color = vertices[provisionIndex].color;
+							vertices[candidateIndex].isVariable = false;
+							upperBound --;
+							numberOfVariableVertices --;
+							didProvideColour = true;
+						}
+					}
+				}
+				
+				// Freeze the variable that tried to provide its colour
+				vertices[provisionIndex].isVariable = false;
+				numberOfVariableVertices --;
+			}
+		}
+		
+		//System.out.println("Upper bound: " + upperBound);
+		return upperBound;
+	}
+	
+	private static int[] append(int newElement, int[] oldArray) {
+		if (oldArray.length == 0) {
+			int[] tmp = {newElement};
+			return tmp;
+		}
+		int[] newArray = new int[oldArray.length+1];
+		for (int i = 0; i < oldArray.length; i++) {
+			newArray[i] = oldArray[i];
+		}
+		newArray[newArray.length-1] = newElement;
+		return newArray;
+	}
+}
